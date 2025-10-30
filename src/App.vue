7ed5311cffe890c4b9cf10ee89e26fcba788e6e4
Maidangtapcode <nhomai2408@gmail.com>
2025-10-30 18:15:27 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+
+}
+</script>
+<template>
+  <h1 class="page">Hello, Vue.js!</h1>
+</template>
+<style>
+.page{
+  max-width: 200px;
+  margin: auto;
+  text-align: center;
+}
+</style>
