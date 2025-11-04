fc01b26e5d44a9e980ea04bfa2f740ddd500ed86
Maidangtapcode <nhomai2408@gmail.com>
2025-11-04 15:57:23 +0700

    them vao contactadd

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="page">
+    <h4>Thêm Liên hệ mới</h4>
+    <ContactForm :contact="contact" @submit:contact="addContact" />
+  </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+  components: { ContactForm },
+  data() {
+    return {
+      contact: {
+        name: "",
+        email: "",
+        address: "",
+        phone: "",
+        favorite: false,
+      },
+    };
+  },
+  methods: {
+    async addContact(data) {
+      try {
+        await ContactService.create(data);
+        alert("Thêm liên hệ mới thành công!");
+        this.$router.push({ name: "contactbook" });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
